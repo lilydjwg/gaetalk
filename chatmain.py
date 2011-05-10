6df7c43c143c0535102567fd43a91a9a13095b9b
lilydjwg <lilydjwg@gmail.com>
2011-05-10 12:51:36 +0800

    初次提交：工作正常

---
+++ chatmain.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+
+import logging
+import datetime
+from google.appengine.api import xmpp
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+import lilytalk
+
+class XMPPSub(webapp.RequestHandler):
+  '''被人加好友了～可能被触发多次'''
+  def post(self):
+    jid = self.request.get('from')
+    u = lilytalk.get_user_by_jid(jid)
+    if u is None:
+      lilytalk.add_user(jid)
+
+class XMPPUnsub(webapp.RequestHandler):
+  def post(self):
+    jid = self.request.get('from')
+    u = lilytalk.get_user_by_jid(jid)
+    if u is not None:
+      u.delete()
+      lilytalk.send_to_all(u'%s 已经离开' % jid)
+      logging.info(u'%s 已经离开' % jid)
+
+class XMPPMsg(webapp.RequestHandler):
+  def post(self):
+    message = xmpp.Message(self.request.POST)
+    lilytalk.handle_message(message)
+
+class XMPPAvail(webapp.RequestHandler):
+  def post(self):
+    '''show 可以是 away、dnd（忙碌）或空（在线）'''
+    jid, resource = self.request.get('from').split('/', 1)
+    status = self.request.get('status')
+    show = self.request.get('show')
+    logging.debug(u'%s 的状态: %s (%s)' % (jid, status, show))
+    show = lilytalk.STATUS_CODE[show]
+    xmpp.send_presence(jid, status=lilytalk.notice)
+    u = lilytalk.get_user_by_jid(jid)
+    if u is not None:
+      u.avail = show
+      u.last_online_date = datetime.datetime.now()
+      u.put()
+    else:
+      logging.info(u'Adding %s (%s)', jid, show)
+      lilytalk.add_user(jid, show)
+
+class XMPPUnavail(webapp.RequestHandler):
+  def post(self):
+    jid, resource = self.request.get('from').split('/', 1)
+    status = self.request.get('status')
+    logging.info(u'%s 下线了' % jid)
+    u = lilytalk.get_user_by_jid(jid)
+    if u is not None:
+      u.avail = lilytalk.OFFLINE
+      u.last_offline_date = datetime.datetime.now()
+      u.put()
+
+application = webapp.WSGIApplication(
+  [
+    ('/_ah/xmpp/subscription/subscribed/', XMPPSub),
+    ('/_ah/xmpp/subscription/unsubscribe/', XMPPUnsub),
+    ('/_ah/xmpp/message/chat/', XMPPMsg),
+    ('/_ah/xmpp/presence/available/', XMPPAvail),
+    ('/_ah/xmpp/presence/unavailable/', XMPPUnavail),
+  ],
+  debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
---
+++ lilytalk.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+
+#FIXME 会有两个不同的资源
+
+import re
+import logging
+import datetime
+from google.appengine.ext import db
+from google.appengine.api import xmpp
+
+notice = u'本群正在内部测试中……'
+helpre = re.compile(r'^\W{0,2}help$')
+
+OFFLINE = u'离线'
+AWAY    = u'离开'
+XAWAY   = u'离开'
+BUSY    = u'忙碌'
+ONLINE  = u'在线'
+CHAT    = u'和我说话吧'
+STATUS_CODE = {
+  '':     ONLINE,
+  'away': AWAY,
+  'dnd':  BUSY,
+  'xa':   XAWAY,
+  'chat': CHAT,
+}
+
+class User(db.Model):
+  jid = db.StringProperty(required=True, indexed=True)
+  nick = db.StringProperty(indexed=True)
+
+  add_date = db.DateTimeProperty(auto_now_add=True)
+  last_online_date = db.DateTimeProperty()
+  last_offline_date = db.DateTimeProperty()
+  last_speak_date = db.DateTimeProperty()
+  msg_count = db.IntegerProperty(required=True, default=0)
+  msg_chars = db.IntegerProperty(required=True, default=0)
+  black_minutes = db.IntegerProperty(required=True, default=0)
+  snooze_minutes = db.IntegerProperty(required=True, default=0)
+  credit = db.IntegerProperty(required=True, default=0)
+
+  avail = db.StringProperty(required=True)
+  is_admin = db.BooleanProperty(required=True, default=False)
+  blocked = db.BooleanProperty(required=True, default=False)
+
+  prefix = db.StringProperty(required=True, default='//')
+  nick_pattern = db.StringProperty(required=True, default='[%s]')
+
+def get_user_by_jid(jid):
+  return User.gql('where jid = :1', jid).get()
+
+def get_member_list():
+  r = []
+  l = User.gql('where avail != :1', OFFLINE)
+  for u in l:
+    r.append(unicode(u.jid))
+  return r
+
+def guess_nick(u):
+  return u.nick or u.jid.split('@')[0].decode('utf-8')
+
+def send_to_all_except_self(jid, message):
+  jids = [x for x in get_member_list() if x != jid]
+  logging.debug(jids)
+  try:
+    xmpp.send_message(jids, message)
+  except xmpp.InvalidJidError:
+    pass
+
+def send_to_all(message):
+  jids = get_member_list()
+  xmpp.send_message(jids, message)
+
+def handle_message(msg):
+  sender = get_user_by_jid(msg.sender.split('/')[0])
+  if sender is None:
+    msg.reply('很抱歉，出错了，请重新添加好友。')
+    return
+  #TODO 管理员命令
+  ch = BasicCommand(msg, sender)
+  if not ch.handled:
+    sender.last_speak_date = datetime.datetime.now()
+    try:
+      sender.msg_count += 1
+      sender.msg_chars += len(msg.body)
+    except TypeError:
+      sender.msg_count = 1
+      sender.msg_chars = len(msg.body)
+    sender.put()
+    msg = '%s %s' % (
+      sender.nick_pattern % guess_nick(sender),
+      msg.body
+    )
+    send_to_all_except_self(sender.jid, msg)
+
+def add_user(jid, show=OFFLINE):
+  u = User(jid=jid, avail=show)
+  u.put()
+  logging.info(u'%s 已经加入' % jid)
+  send_to_all_except_self(jid, u'%s 已经加入' % jid)
+  xmpp.send_presence(jid, status=notice)
+  xmpp.send_message(jid, u'欢迎 %s 加入～' % jid.split('@')[0])
+
+class BasicCommand:
+  handled = True
+
+  def __init__(self, msg, sender):
+    self.sender = sender
+    self.msg = msg
+
+    if helpre.match(msg.body):
+      self.do_help()
+    elif msg.body.startswith(sender.prefix):
+      cmd = msg.body[len(sender.prefix):].split()
+      try:
+        getattr(self, 'do_' + cmd[0])(cmd[1:])
+      except AttributeError:
+        msg.reply(u'错误：未知命令 %s' % cmd[0])
+      except UnicodeEncodeError:
+        msg.reply(u'错误：命令名解码失败。此问题在 GAE 升级其 Python 到 3.x 后方能解决。')
+    else:
+      self.handled = False
+
+  def do_online(self, args):
+    '''显示在线成员列表'''
+    r = [u'在线成员列表:']
+    l = User.gql('where avail != :1', OFFLINE)
+    for u in l:
+      m = guess_nick(u)
+      status = u.avail
+      if status != u'在线':
+        m += u' (%s)' % status
+      r.append(unicode('* ' + m))
+    self.msg.reply(u'\n'.join(r).encode('utf-8'))
+
+  def do_nick(self, args):
+    '''更改昵称，需要一个参数'''
+    if len(args) != 1:
+      self.msg.reply('错误：请给出你想到的昵称（不能包含空格）')
+      return
+
+    q = User.gql('where nick = :1', args[0]).get()
+    if q is not None:
+      self.msg.reply('错误：该昵称已被使用，请使用其它昵称')
+    else:
+      old_nick = guess_nick(self.sender)
+      self.sender.nick = args[0]
+      self.sender.put()
+      send_to_all_except_self(self.sender.jid,
+        (u'%s 的昵称改成了 %s' % (old_nick, args[0])).encode('utf-8'))
+      self.msg.reply('昵称更改成功！')
+
+  def do_help(self, args=None):
+    '''显示本帮助'''
+    doc = [u'命令指南 (使用时请加上你的命令前缀，默认为 // )']
+    for c, f in self.__class__.__dict__.items():
+      if c.startswith('do_'):
+        doc.append(u'%s: %s' % (c[3:], f.__doc__.decode('utf-8')))
+    self.msg.reply(u'\n'.join(doc).encode('utf-8'))
+

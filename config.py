d0144f35779cd1f84996bf837e2164a17b71ae9a
lilydjwg <lilydjwg@gmail.com>
2011-06-04 18:34:44 +0800

    屏蔽离开时自动回复的消息

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ nick_can_change = True #是否允许多次更改昵称
 nick_maxlen = 16 #单位是字节
 warnGtalk105 = True
 root = 'lilydjwg@gmail.com'
+
+# 离开时某些客户端自动发送的消息（全文）
+blocked_away_messages = (
+  "I'm currently away and will reply as soon as I return to eBuddy on my iPod touch",
+)

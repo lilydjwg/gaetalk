24773b83e2ec268665b83ab55e9745fc1ffb9573
lilydjwg <lilydjwg@gmail.com>
2011-06-05 20:56:20 +0800

    添加安装与配置说明

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python2
 # vim:fileencoding=utf-8
 
+# 时区
 timezoneoffset = 8
+# 默认的命令前缀
 default_prefix = '-'
+# 除了 Unicode 分类为“字母”的字符外，昵称里还允许哪些字符。注意即使指定空白
+# 符昵称中也不能包含之
 allowedSymbolInNick = u'-_@.™'
-nick_can_change = True #是否允许多次更改昵称
-nick_maxlen = 16 #单位是字节
+# 是否允许多次更改昵称
+nick_can_change = True
+# 单位是字节。一个汉字为 3 字节
+nick_maxlen = 16
+# Gtalk 官方中文版使用非加密的协议。检测到 Gtalk 官方中文版用户时要不要提示之。
 warnGtalk105 = True
+# root 用户，请指定群主的 JID。
 root = 'lilydjwg@gmail.com'
 
 # 离开时某些客户端自动发送的消息（全文）
